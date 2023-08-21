193f034e0870639e12b4c66f1cbf11245f6cdd33
khanshanawaz10 <shanawaz.khan@nivesh.com>
2023-08-21 09:27:43 +0530

    secound COMMIT

--- app/test.py
+++ app/test.py
@@ -7,17 +7,17 @@ class TestDockerapp(unittest.TestCase):
         self.app = app.app.test_client()
 
     def test_save_value(self):
-        response = self.app.post('/', data=dict(submit='save', key='3', cache_value='three'))
+        response = self.app.post('/', data=dict(submit='save', key='2', cache_value='two'))
         assert response.status_code == 200
-        assert b'3' in response.data
-        assert b'three' in response.data
+        assert b'2' in response.data
+        assert b'two' in response.data
 
     def test_load_value(self):
-        self.app.post('/', data=dict(submit='save', key='3', cache_value='three'))
-        response = self.app.post('/', data=dict(submit='load', key='3'))
+        self.app.post('/', data=dict(submit='save', key='2', cache_value='two'))
+        response = self.app.post('/', data=dict(submit='load', key='2'))
         assert response.status_code == 200
-        assert b'3' in response.data
-        assert b'three' in response.data
+        assert b'2' in response.data
+        assert b'two' in response.data
 
 if __name__=='__main__':
     unittest.main()
